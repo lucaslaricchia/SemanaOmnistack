76767aee986cb42b0ee7f9e7c1a7294659979c7a
Lucas <lucaslaricchia@gmail.com>
2020-01-22 15:08:26 -0300

    React Adicionado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,12 @@ const DevController = require('./controllers/DevController');
 const SearchController = require('./controllers/SearchController');
 const routes = Router();
 
+
+routes.get('', (request, response) => {
+
+    return response.json({ message : "HELLOWORLD"});
+
+});
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 routes.get('/search', SearchController.index);
