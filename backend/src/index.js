6eeec17ba09e17a7ad8b0b8ffe091051d8e14722
Lucas Laricchia <lucaslaricchia@gmail.com>
2020-01-23 22:46:24 -0300

    Adicionado recurso para cadastrar devs e listagem de devs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 const express = require('../node_modules/express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(routes);
 
-mongoose.connect('mongodb+srv://lucaslaricchia:123654@cluster0-jrvdj.mongodb.net/week10?retryWrites=true&w=majority', {
+
+mongoose.connect('mongodb+srv://lucaslaricchia:123654@clusterinicial-jrvdj.mongodb.net/test?retryWrites=true&w=majority', {
    useNewUrlParser: true,
    useUnifiedTopology: true,
 });
--- web/src/App.js
+++ web/src/App.js
@@ -3,11 +3,16 @@ import './global.css';
 import './Sidebar.css';
 import './App.css';
 import './Main.css';
-
+import api from'./services/api';
 
 
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  const[github_username, setGithub_username] = useState('');
+  const[techs, setTechs] = useState('');
+
   const[latitude, setLatitude] = useState('');
   const[longitude, setLongitude] = useState('');
 
@@ -26,20 +31,53 @@ function App() {
         timeout: 30000,
       }
     )
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(e){
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+
+    console.log(response.data);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
           <label htmlFor="github_username">Usuário do Github</label>
-          <input name="github_username" id="github_username" required />
+          <input 
+          name="github_username" 
+          id="github_username" 
+          required 
+          value={github_username}
+          onChange={e => setGithub_username(e.target.value)}
+          />
           </div>
           <div className="input-block">
           <label htmlFor="techs">Tecnologias</label>
-          <input name="techs" id="techs" required />
+          <input 
+          name="techs" 
+          id="techs" 
+          required 
+          value={techs}
+          onChange={e => setTechs(e.target.value)}
+          />
           </div>
           <div className="input-group">
             <div className="input-block">
@@ -71,53 +109,20 @@ function App() {
 
       <main>
         <ul>
-          <li className="dev-item">
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
             <header>
-              <img src="https://www.gettyimages.pt/gi-resources/images/RoyaltyFree/Apr17Update/ColourSurge1.jpg" />
+              <img src={dev.avatar_url} alt={dev.name}/>
               <div className="user-info">
-                <strong>Lucas Laricchia</strong>
-                <span>C++, Javascript, NodeJS</span>
+                <strong>{dev.name}</strong>
+                <span>{dev.techs.join(', ')}</span>
               </div>
             </header>
-            <p>Mechatronics Engenieer</p>
-            <a href="twitch.tv/toalhinhadota">Acessar Twtich</a>
+            <p>{dev.bio}</p>
+            <a href={`http://github.com/${dev.github_username}`}>Acessar GitHub</a>
           </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://www.gettyimages.pt/gi-resources/images/RoyaltyFree/Apr17Update/ColourSurge1.jpg"/>
-              <div className="user-info">
-                <strong>Lucas Laricchia</strong>
-                <span>C++, Javascript, NodeJS</span>
-              </div>
-            </header>
-            <p>Mechatronics Engenieer</p>
-            <a href="twitch.tv/toalhinhadota">Acessar Twtich</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://www.gettyimages.pt/gi-resources/images/RoyaltyFree/Apr17Update/ColourSurge1.jpg"/>
-              <div className="user-info">
-                <strong>Lucas Laricchia</strong>
-                <span>C++, Javascript, NodeJS</span>
-              </div>
-            </header>
-            <p>Mechatronics Engenieer</p>
-            <a href="twitch.tv/toalhinhadota">Acessar Twtich</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://www.gettyimages.pt/gi-resources/images/RoyaltyFree/Apr17Update/ColourSurge1.jpg"/>
-              <div className="user-info">
-                <strong>Lucas Laricchia</strong>
-                <span>C++, Javascript, NodeJS</span>
-              </div>
-            </header>
-            <p>Mechatronics Engenieer</p>
-            <a href="twitch.tv/toalhinhadota">Acessar Twtich</a>
-          </li>
+          ))}
+          
         </ul>
       </main>
     </div>
